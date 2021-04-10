5a101dc539b864fed64e875f5876a4777607211d
Garrett Higginbotham <ghiggie@uab.edu>
2021-04-10 10:12:24 -0500

    Implemented skeleton structure for fundamental methods.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Milestone_Project_1
  First Milestone Project for Complete Python Course on Udemy
+
+In this milestone project, I will create a program for creating a movie database.
---
+++ movie_library.py
@@ -0,0 +1,35 @@
+movie_list = []
+
+def add_movie():
+    pass
+
+
+def find_movie(title):
+    pass
+
+
+def remove_movie(title):
+    pass
+
+
+def edit_metadata():
+    pass
+
+
+def print_movie():
+    pass
+
+
+
+
+
+def menu():
+    pass
+
+
+def main():
+    menu()
+
+
+if __name__ == "__main__":
+    main()
